274064228bd77a44b13eeb5cdea429ca24a585f3
YunYouJun <me@yunyoujun.cn>
2026-08-30 23:53:14 +0800

    docs: add marketplace hero and showcase

--- docs/.vitepress/components.d.ts
+++ docs/.vitepress/components.d.ts
@@ -13,5 +13,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     DocsProjectCard: typeof import('./../../node_modules/.pnpm/@yunyoujun+docs@0.1.14_vite@8.2.2_@types+node@26.3.0_esbuild@0.28.2_jiti@2.7.0_tsx@4.23.12_yaml@2.9.0_/node_modules/@yunyoujun/docs/client/components/DocsProjectCard.vue')['default']
     DocsProjectList: typeof import('./../../node_modules/.pnpm/@yunyoujun+docs@0.1.14_vite@8.2.2_@types+node@26.3.0_esbuild@0.28.2_jiti@2.7.0_tsx@4.23.12_yaml@2.9.0_/node_modules/@yunyoujun/docs/client/components/DocsProjectList.vue')['default']
+    MarketplacePage: typeof import('./theme/components/MarketplacePage.vue')['default']
   }
 }
